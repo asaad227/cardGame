21b9799fc704aaa611377992edbbf188b95a8be2
asaad227 <a.saad227@yahoo.com>
2022-10-19 21:58:19 +0100

    score and round display

--- cardGame.js
+++ cardGame.js
@@ -1,8 +1,14 @@
+var round = [];
+var count = [];
+// its need to declared outside the function scope to keep adding up the score and round
+var playerPick;
+var computerPick;
+
 function cardGame(player, computer) {
   const plusSet = ["2", "3", "4", "5", "6"];
   const minusSet = ["7", "8", "9", "10"];
   const doubleDown = ["A", "K", "J", "Q"];
-  const count = [];
+ 
 
   for (let i = 0; i < player.length; i++) {
     const playerPlus = plusSet.includes(player[i]);
@@ -15,17 +21,17 @@ function cardGame(player, computer) {
     (playerPlus && computerMinus) ||
     (playerDouble && computerPlus) ||
     (playerDouble && computerMinus)
-      ? count.push(1)
+      ? count.push(1)&& round.push(1)
       : count;
     (computerPlus && playerMinus) ||
     (computerDouble && playerPlus) ||
     (computerDouble && playerMinus)
-      ? count.push(-1)
+      ? count.push(-1)&& round.push(1)
       : count;
     (playerDouble && computerDouble) ||
     (playerMinus && computerMinus) ||
     (playerPlus && computerPlus)
-      ? count.push(0)
+      ? count.push(0)&& round.push(1)
       : count;
 
    
@@ -33,7 +39,9 @@ function cardGame(player, computer) {
 
   const result = count.reduce((a, b) => a + b, 0);
   console.log(result);
- 
+  console.log(round, count)
+  document.querySelector(".round").innerHTML= round? `Round: ${round.length}`:`Round: 0`;
+  document.querySelector(".total").innerHTML= count? `Score: ${result}`: `Score: 0`;
 
   return result
     ? result > 0
@@ -59,19 +67,19 @@ const cards = [
   "3",
   "2",
 ];
-var playerPick
+
 function playerP(){
     const d = Math.ceil(Math.random() * 12);
     playerPick = [cards[d]];
     document.querySelector(".playerPick").innerHTML =  `Player selection: ${playerPick}`;
-    console.log(playerPick)
+ 
 }
-var computerPick
+
 function computerP(){
     const i = Math.ceil(Math.random() * 12);
     computerPick = [cards[i]];
     document.querySelector(".computerPick").innerHTML =  `Computer selection: ${computerPick}`
-    console.log(computerPick);
+   
 }
 
 function getResult(){
@@ -87,11 +95,11 @@ function reset(){
   document.querySelector(".computerPick").innerHTML =  "";
   document.querySelector(".playerPick").innerHTML =  "";
 }
-const btnPlayer = document.querySelector(".playerSelection");
 
+const btnPlayer = document.querySelector(".playerSelection");
 const btnGame = document.querySelector(".cardGame");
 const btnReset = document.querySelector(".reset");
 btnPlayer.addEventListener("click", playerP);
-
 btnGame.addEventListener("click", getResult);
 btnReset.addEventListener("click", reset);
+
